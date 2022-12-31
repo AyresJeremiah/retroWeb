1828b771fb49de02721461b4810d213a11f274db
Jeremiah J Ayres <ayresjeremiah@gmail.com>
2022-12-31 16:45:38 -0500

    Regex

--- html/fileUpload.php
+++ html/fileUpload.php
@@ -1,15 +1,20 @@
 <?php
 
 if (isset($_FILES['file']['name'])&& isset($_REQUEST['console'])) { //file UPLOAD
-    $filename = $_FILES['file']['name'];
-    $romsPath = "/home/jeremiah/RetroPie/roms/" . $_REQUEST['console'] . "/" .  $filename;
-    $location="/home/jeremiah/RetroPie/roms/" . $_REQUEST['console'];
+    $console=preg_replace("/[^0-9a-zA-Z\-\_\ \.\:\,\'\"\?\/]/","",$_REQUEST['console']);
+    $console = trim($console);
+    $console = stripslashes($console);
+    $console = htmlspecialchars($console);
 
-    /* Location */
-    
+    $filename=preg_replace("/[^0-9a-zA-Z\-\_\ \.\:\,\'\"\?\/]/","",$_FILES['file']['name']);
+    $filename = trim($filename);
+    $filename = stripslashes($filename);
+    $filename = htmlspecialchars($filename);
+
+    $romsPath = "/home/retro/RetroPie/roms/" . $console . "/";
+    $location="/home/retro/RetroPie/roms/" . $console;
     /* Upload file */
-    if(move_uploaded_file($_FILES['file']['tmp_name'], $romsPath )) {
-        shell_exec("unzip $romsPath");
+    if(move_uploaded_file($_FILES['file']['tmp_name'], $romsPath.$filename)){
         echo "1";
     }else{
         echo "0";
